117a8e09c8fcb037aa3e197423e59d2b86ad44b3
Vinton Teo <taulago@ymail.com>
2019-07-31 10:02:50 -0600

    Create function to fetch animal data.

---
+++ src/apiCalls.js
@@ -0,0 +1,10 @@
+export const getAnimals = () => {
+  try {
+    return fetch('http://localhost:3001/api/v1/rescue-animals')
+      .then(response => response.json())
+      .then(data => data)
+      .catch(error => error.message);
+  } catch ({ message }) {
+    throw Error(message);
+  }
+};
