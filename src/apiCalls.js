44e51428767d36896d7789d372a6ca34f1ac0809
Vinton Teo <taulago@ymail.com>
2019-07-31 11:36:25 -0600

    Create function to post new donation to api.

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -19,3 +19,17 @@ export const getDonations = () => {
     throw Error(message);
   }
 };
+
+export const postNewDonation = donation => {
+  const url = 'http://localhost:3001/api/v1/donations/';
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ ...donation })
+  };
+  try {
+    return fetch(url, options).catch(error => error.message);
+  } catch ({ message }) {
+    throw new Error(message);
+  }
+};
