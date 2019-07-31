b2144fdae5ae875b82957791cca198237d126313
Vinton Teo <taulago@ymail.com>
2019-07-31 11:20:07 -0600

    Add new addDonation action to donationsReducer.

--- src/reducers/donationsReducer.js
+++ src/reducers/donationsReducer.js
@@ -2,6 +2,8 @@ export const donationsReducer = (state = [], action) => {
   switch (action.type) {
     case 'STORE_DONATIONS':
       return action.donations;
+    case 'ADD_DONATION':
+      return [...state, action.donation];
     default:
       return state;
   }
